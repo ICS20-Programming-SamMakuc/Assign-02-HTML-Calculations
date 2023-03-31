ef7e864a9434e1668c73ab61ba93d3187a8e34da
SamMakuc <124847549+SamMakuc@users.noreply.github.com>
2023-03-31 19:51:42 +0000

    W

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@
 
 function enterClicked() {
   // Get the temperature input in Celsius and Fahrenheit
-  var tempCelsius = document.getElementById("temp-celsius").value;
-  var tempFahrenheit = document.getElementById("temp-fahrenheit").value;
+  let tempCelsius = document.getElementById("temp-celsius").value;
+  let tempFahrenheit = document.getElementById("temp-fahrenheit").value;
 
   // Check which input is empty and calculate the other
   if (tempCelsius !== "") {
-    var convertedTemp = (tempCelsius * 9/5) + 32;
+    let convertedTemp = (tempCelsius * 9/5) + 32;
     document.getElementById("tempAfterConversionF").innerHTML = "Temperature in Fahrenheit: " + convertedTemp.toFixed(2) + "°F (Reload web page before another use)";
   } else if (tempFahrenheit !== "") {
-    var convertedTemp = (tempFahrenheit - 32) * 5/9;
+    let convertedTemp = (tempFahrenheit - 32) * 5/9;
     document.getElementById("tempAfterConversionC").innerHTML = "Temperature in Celsius: " + convertedTemp.toFixed(2) + "°C (Reload web page before another use)";
   } else {
     alert("Please enter a temperature.");
